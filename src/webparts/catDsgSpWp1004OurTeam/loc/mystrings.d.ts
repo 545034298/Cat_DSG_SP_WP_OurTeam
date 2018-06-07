7ca33265c3a4566b2e61461066415479b4fc939f
Roger Zhang <zhangqinyuan2013@live.cn>
2018-06-07 10:23:26 +0800

    Initialize Our Team SPFX Web Part

---
+++ src/webparts/catDsgSpWp1004OurTeam/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICatDsgSpWp1004OurTeamWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CatDsgSpWp1004OurTeamWebPartStrings' {
+  const strings: ICatDsgSpWp1004OurTeamWebPartStrings;
+  export = strings;
+}
